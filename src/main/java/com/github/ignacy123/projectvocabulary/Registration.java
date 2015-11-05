5867e7d3c14ca125336e948dc7011f110cf45de9
ignacy123 <ignacy.buczek@onet.pl>
2015-11-05 17:16:01 +0100

    Introducing list

--- src/main/java/com/github/ignacy123/projectvocabulary/Registration.java
+++ src/main/java/com/github/ignacy123/projectvocabulary/Registration.java
@@ -1,6 +1,8 @@
 package com.github.ignacy123.projectvocabulary;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by ignacy on 04.11.15.
@@ -9,6 +11,13 @@ public class Registration {
 
 
     public static void main(String[] args) throws IOException {
+        String homePath = System.getProperty("vocabulary.home");
+        Objects.requireNonNull(homePath);
+        File home = new File(homePath);
+        if (!home.exists() || !home.isDirectory()) {
+            throw new RuntimeException("Wrong home dir.");
+        }
+        File userData = getOrCreateUserData(home);
         String name = "";
         String password;
         String confirm;
@@ -29,7 +38,6 @@ public class Registration {
             user.setLogin(name);
             user.setPassword(password);
             user.setEmail(email);
-            File userData = File.createTempFile(name + "'s data", ".txt");
             ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(userData));
             outputStream.writeObject(user);
             outputStream.flush();
@@ -41,5 +49,17 @@ public class Registration {
             System.out.println("Wrong password confirmation. Try again");
         }
 
+    }
+
+    private static File getOrCreateUserData(File home) throws IOException {
+        File userData = new File(home, "userData.obj");
+        if (!userData.exists()) {
+            userData.createNewFile();
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(userData));
+            outputStream.writeObject(new ArrayList<User>());
+            outputStream.flush();
+            outputStream.close();
+        }
+        return userData;
     }
 }
