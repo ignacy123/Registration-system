b536ea083cd2b5d741a23999e6912b168286ffdc
ignacy123 <ignacy.buczek@onet.pl>
2015-11-26 15:47:17 +0100

    New structure

--- src/main/java/com/github/ignacy123/projectvocabulary/Application.java
+++ src/main/java/com/github/ignacy123/projectvocabulary/Application.java
@@ -6,32 +6,16 @@ import java.io.*;
  * Created by ignacy on 12.11.15.
  */
 public class Application {
-    private final InputStream inputStream;
-    private final PrintStream printStream;
 
-    public Application(InputStream inputStream, PrintStream printStream) {
-        this.inputStream = inputStream;
 
-        this.printStream = printStream;
-    }
+    public String run() {
 
-    public void run() {
-        printStream.println("1. Zarejestruj");
-        printStream.println("0. Koniec");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        try {
-            String line = reader.readLine();
-            if (line.equals("0")) {
-                printStream.println("Do zobaczenia!");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        return "1. Zarejestruj\n0. Koniec\n";
     }
 
     public static void main(String[] args) {
-        Application application = new Application(System.in, System.out);
+        Application application = new Application();
         application.run();
     }
 }
--- src/test/java/com/github/ignacy123/projectvocabulary/ApplicationTest.java
+++ src/test/java/com/github/ignacy123/projectvocabulary/ApplicationTest.java
@@ -12,38 +12,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by ignacy on 12.11.15.
  */
 public class ApplicationTest {
-    private ByteArrayOutputStream applicationOutput;
-    private PrintStream applicationInput;
-    private Application application;
 
-    @Before
-    public void before() throws IOException {
-        applicationOutput = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(applicationOutput);
-        PipedOutputStream pipedOutputStream = new PipedOutputStream();
-        PipedInputStream pipedInputStream = new PipedInputStream(pipedOutputStream);
-        applicationInput = new PrintStream(pipedOutputStream);
-        application = new Application(pipedInputStream, printStream);
-    }
 
     @Test
     public void printsMenu() throws IOException {
+        Application application = new Application();
+        String output = application.run();
+        assertThat(output, is("1. Zarejestruj\n0. Koniec\n"));
 
-
-        applicationInput.println("test");
-        application.run();
-
-        String output = applicationOutput.toString();
-        assertThat(output, equalTo("1. Zarejestruj\n0. Koniec\n"));
-    }
-
-    @Test
-    public void closesProperly() throws IOException {
-
-        applicationInput.println("0");
-        application.run();
-
-        String output = applicationOutput.toString();
-        assertThat(output, equalTo("1. Zarejestruj\n0. Koniec\nDo zobaczenia!\n"));
     }
 }
